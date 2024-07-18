9c7d964abd3ee88da98c233d04e0326d6c5df462
Wilson Armando Nieva Caicedo <124019562+wanc142029@users.noreply.github.com>
2024-07-18 05:58:02 -0500

    Add files via upload

---
+++ PracticaJavaScritp/JavaScritpt/Ejecicios_01.js
@@ -0,0 +1,77 @@
+// 1. Imprime "Hola, mundo" en la consola.
+
+console.log("Hola mundo");
+
+// 2. Imprime tu nombre en la consola.
+
+console.log("Wilson Armando Nieva Caicedo");
+
+// 3. Muestra un mensaje de error en la consola.
+
+console.error("Este es un error causado a proposito");
+
+// 4. Imprime el resultado de 5 + 3 en la consola.
+
+console.log(5 + 3);
+
+// 5. Imprime el resultado de 10 - 7 en la consola.
+
+console.log(10 - 7);
+
+// 6. Declara una variable nombrecon tu nombre y muéstralo en la consola.
+
+let nombre = "Wilson Armando Nieva Caicedo";
+console.log(nombre);
+
+// 7. Declara una variable edad con tu edad y muéstralo en la consola.
+
+let edad = 34;
+console.log(edad);
+
+// 8. Imprime el resultado de 8*5 en la consola.
+
+console.log(8 * 5);
+
+// 9. Imprime el resultado de 20/4 en la consola.
+
+console.log(20 / 4);
+
+//10. Declara dos variables ay bcon valores numéricos y muestra su suma en la consola.
+
+let a = 20;
+let b = 30;
+let resul = a + b;
+console.log(resul)
+
+// 11. Imprime el resultado de la concatenación de dos cadenas de texto.
+
+let texto1 = "Mi nombre es: ";
+let texto2 = "Wilson Armando Nieva Caicedo";
+console.log(texto1, texto2);
+
+// 12. Pide al usuario que ingrese su nombre y muestre su nombre en la consola.
+
+let usuario = prompt("Ingrese su nombre: ");
+console.log(usuario);
+
+// 13. Solicita al usuario que ingrese su edad y luego muestra un mensaje en la consola con esa edad.
+
+let edad1 = prompt("Por favor ingrese su edad: ");
+console.log(edad1);
+
+// 14. Pide al usuario que ingrese dos números y muestre la suma de estos números en la consola.
+
+let numero1 = Number(prompt("Vamos a realizar la suma de dos numeros enteros, por favor digita el primer numero"));
+let numero2 = Number(prompt("Ingresa el segundo numero"));
+let result = numero1 + numero2;
+console.log(result);
+
+// 15. Solicita al usuario que ingrese su color favorito y muestre un mensaje con ese color en la consola.
+
+let color = prompt("Por favor escribe tu color fvorito: ");
+console.log(color);
+
+// 16. Pide al usuario que ingrese una frase y muestra esa frase en la consola.
+
+let frase = prompt("Escribe una frase que te guste: ")
+console.log(frase)
